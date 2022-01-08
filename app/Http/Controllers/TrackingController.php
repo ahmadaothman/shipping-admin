ebd3b57daa8f4b27ca58abb1543e5c275abf1574
ahmadaothman <ahmadaothman96@gmail.com>
2022-01-08 22:06:31 +0200

    sa

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -9,9 +9,18 @@ use App\Models\ShipmentHistory;
 class TrackingController extends Controller
 {
     public function index(Request $request){
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+                header('Access-Control-Allow-Origin: *');
+                header('Access-Control-Allow-Headers: Authorization,content-type');
+                header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+            }
+            exit;
+        }
         header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: *');
-        header('Access-Control-Allow-Headers: *');
+        
+        header('Access-Control-Allow-Headers: content-type,authorization');
+        header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
         
         $shipment = Shipment::where('tracking_number',$request->get('tracking_number'))->first();
         $shipment_history = ShipmentHistory::where('shipment_id',$shipment->id)->get();
