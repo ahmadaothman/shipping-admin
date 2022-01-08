066d2f8a48345b2b85ec031d7b55681616e650b8
ahmadaothman <ahmadaothman96@gmail.com>
2022-01-08 22:04:10 +0200

    .

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -9,6 +9,10 @@ use App\Models\ShipmentHistory;
 class TrackingController extends Controller
 {
     public function index(Request $request){
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: *');
+        header('Access-Control-Allow-Headers: *');
+        
         $shipment = Shipment::where('tracking_number',$request->get('tracking_number'))->first();
         $shipment_history = ShipmentHistory::where('shipment_id',$shipment->id)->get();
         
