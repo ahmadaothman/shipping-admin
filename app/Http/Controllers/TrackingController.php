daafb85810868798afbcabbb4d615db27ef6dc3a
ahmadaothman <ahmadaothman96@gmail.com>
2022-01-08 22:08:41 +0200

    Update TrackingController.php

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -9,6 +9,8 @@ use App\Models\ShipmentHistory;
 class TrackingController extends Controller
 {
     public function index(Request $request){
+        $data = array();
+
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
                 header('Access-Control-Allow-Origin: *');
@@ -21,14 +23,22 @@ class TrackingController extends Controller
         
         header('Access-Control-Allow-Headers: content-type,authorization');
         header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
-        
-        $shipment = Shipment::where('tracking_number',$request->get('tracking_number'))->first();
-        $shipment_history = ShipmentHistory::where('shipment_id',$shipment->id)->get();
-        
-        $data = array();
-        $data['shipment'] = $shipment;
-        $data['history'] = $shipment_history;
 
-        return $data;
+        $shipment = Shipment::where('tracking_number',$request->get('tracking_number'))->first();
+        if($shipment){
+            $shipment_history = ShipmentHistory::where('shipment_id',$shipment->id)->get();
+        
+      
+            $data['success'] = true;
+            $data['shipment'] = $shipment;
+            $data['history'] = $shipment_history;
+    
+            return $data;
+        }else{
+            $data['success'] = false;
+            $data['message'] = 'Shipment not found!';
+            return $data;
+        }
+     
     }
 }
